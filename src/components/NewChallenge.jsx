788396c2ac5c2a3164a4c7ffd2849958800f58df
Michael Thinh <michaelthinh1008@gmail.com>
2023-11-29 18:01:43 +0700

    add: main app animation

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -1,80 +1,116 @@
-import { useContext, useRef, useState } from 'react';
+import { useContext, useRef, useState } from "react";
+import { motion, useAnimate, stagger } from "framer-motion";
 
-import { ChallengesContext } from '../store/challenges-context.jsx';
-import Modal from './Modal.jsx';
-import images from '../assets/images.js';
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import Modal from "./Modal.jsx";
+import images from "../assets/images.js";
 
 export default function NewChallenge({ onDone }) {
-  const title = useRef();
-  const description = useRef();
-  const deadline = useRef();
+    const title = useRef();
+    const description = useRef();
+    const deadline = useRef();
 
-  const [selectedImage, setSelectedImage] = useState(null);
-  const { addChallenge } = useContext(ChallengesContext);
+    const [scope, animate] = useAnimate();
 
-  function handleSelectImage(image) {
-    setSelectedImage(image);
-  }
+    const [selectedImage, setSelectedImage] = useState(null);
+    const { addChallenge } = useContext(ChallengesContext);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const challenge = {
-      title: title.current.value,
-      description: description.current.value,
-      deadline: deadline.current.value,
-      image: selectedImage,
-    };
-
-    if (
-      !challenge.title.trim() ||
-      !challenge.description.trim() ||
-      !challenge.deadline.trim() ||
-      !challenge.image
-    ) {
-      return;
+    function handleSelectImage(image) {
+        setSelectedImage(image);
     }
 
-    onDone();
-    addChallenge(challenge);
-  }
+    function handleSubmit(event) {
+        event.preventDefault();
+        const challenge = {
+            title: title.current.value,
+            description: description.current.value,
+            deadline: deadline.current.value,
+            image: selectedImage,
+        };
 
-  return (
-    <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
-        <p>
-          <label htmlFor="title">Title</label>
-          <input ref={title} type="text" name="title" id="title" />
-        </p>
+        if (
+            !challenge.title.trim() ||
+            !challenge.description.trim() ||
+            !challenge.deadline.trim() ||
+            !challenge.image
+        ) {
+            console.log("cc");
+            animate(
+                "input,textarea",
+                { x: [-10, 0, 10, 0] },
+                { type: "keyframes", duration: 0.2, delay: stagger(0.05) }
+            );
+            return;
+        }
 
-        <p>
-          <label htmlFor="description">Description</label>
-          <textarea ref={description} name="description" id="description" />
-        </p>
+        onDone();
+        addChallenge(challenge);
+    }
 
-        <p>
-          <label htmlFor="deadline">Deadline</label>
-          <input ref={deadline} type="date" name="deadline" id="deadline" />
-        </p>
+    return (
+        <Modal title="New Challenge" onClose={onDone}>
+            <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
+                <p>
+                    <label htmlFor="title">Title</label>
+                    <input ref={title} type="text" name="title" id="title" />
+                </p>
 
-        <ul id="new-challenge-images">
-          {images.map((image) => (
-            <li
-              key={image.alt}
-              onClick={() => handleSelectImage(image)}
-              className={selectedImage === image ? 'selected' : undefined}
-            >
-              <img {...image} />
-            </li>
-          ))}
-        </ul>
+                <p>
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        ref={description}
+                        name="description"
+                        id="description"
+                    />
+                </p>
 
-        <p className="new-challenge-actions">
-          <button type="button" onClick={onDone}>
-            Cancel
-          </button>
-          <button>Add Challenge</button>
-        </p>
-      </form>
-    </Modal>
-  );
+                <p>
+                    <label htmlFor="deadline">Deadline</label>
+                    <input
+                        ref={deadline}
+                        type="date"
+                        name="deadline"
+                        id="deadline"
+                    />
+                </p>
+
+                <motion.ul
+                    id="new-challenge-images"
+                    variants={{
+                        visible: {
+                            transition: {
+                                staggerChildren: 0.05,
+                            },
+                        },
+                    }}
+                >
+                    {images.map((image) => (
+                        <motion.li
+                            variants={{
+                                start: { opacity: 0, scale: 0.5 },
+                                visible: { opacity: 1, scale: [0.8, 1.3, 1] },
+                                end: {},
+                            }}
+                            exit={{ opacity: 1, scale: 1 }}
+                            transition={{ type: "spring" }}
+                            key={image.alt}
+                            onClick={() => handleSelectImage(image)}
+                            className={
+                                selectedImage === image ? "selected" : undefined
+                            }
+                        >
+                            <img {...image} />
+                        </motion.li>
+                    ))}
+                </motion.ul>
+
+                <p className="new-challenge-actions">
+                    <button type="button" onClick={onDone}>
+                        Cancel
+                    </button>
+                    <button>Add Challenge</button>
+                </p>
+            </form>
+        </Modal>
+    );
 }
